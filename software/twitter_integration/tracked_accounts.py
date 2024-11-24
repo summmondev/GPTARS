ec18be26c3dfbe57ada3b5f912f623487e219755
Charlie Diaz <126284061+summmondev@users.noreply.github.com>
2024-11-24 03:28:14 +0200

    Update tracked_accounts.py

--- software/twitter_integration/tracked_accounts.py
+++ software/twitter_integration/tracked_accounts.py
@@ -1,1 +1,19 @@
+tracked_accounts = [
+    "@amplifiedamp",
+    "@Shoggoth_SOL",
+    "@aiwdaddyissues",
+    "@AIHegemonyMemes",
+    "@project_89",
+    "@plastic_labs",
+    "@vvaifudotfun",
+    "@dasha_terminal",
+    "@pumpdotfun",
+    "@FartCoinOfSOL",
+    "@divinediarrhea",
+    "@ai16zdao",
+    "@degenspartanai",
+    "@pmairca",
+    "@truth_terminal",
+    "@0xzerebro"
+]
 
