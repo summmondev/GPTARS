1f75883b5f12609a4651d0471e8646c308cc52f4
Charlie Diaz <126284061+summmondev@users.noreply.github.com>
2024-11-24 03:29:48 +0200

    Update movement.py

--- software/hardware_control/movement.py
+++ software/hardware_control/movement.py
@@ -1,1 +1,11 @@
+import Adafruit_PCA9685
+import time
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(60)
+
+def move_forward():
+    pwm.set_pwm(0, 0, 300)
+    time.sleep(0.5)
+    pwm.set_pwm(0, 0, 400)
 
