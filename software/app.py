ee8095906dd8710b4eb737bc5617e6a07558c5b8
Charlie Diaz <126284061+summmondev@users.noreply.github.com>
2024-11-24 03:24:31 +0200

    Update app.py

--- software/app.py
+++ software/app.py
@@ -1,1 +1,8 @@
+from twitter_integration.twitter_bot import start_twitter_bot
+from hardware_control.movement import perform_movement_tests
+
+if __name__ == "__main__":
+    print("Starting GPTARS...")
+    start_twitter_bot()
+    perform_movement_tests()
 
